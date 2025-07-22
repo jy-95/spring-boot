a50212a834cf4248b97a23d385664eaa1c5adead
jy-95 <jiiyon.lee@gmail.com>
2025-07-22 17:44:58 +0900

    viewCount

--- springEx5/src/main/java/net/dsa/web5/controller/BoardController.java
+++ springEx5/src/main/java/net/dsa/web5/controller/BoardController.java
@@ -1,12 +1,21 @@
 package net.dsa.web5.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.dsa.web5.dto.BoardDTO;
 import net.dsa.web5.service.BoardService;
 
 @Slf4j
@@ -26,5 +35,70 @@ public class BoardController {
 	@GetMapping("write")
 	public String write() {
 		return "board/writeForm";
+	}
+	
+	/**
+	 * 글 저장
+	 * @param boardDTO 작성한 글정보
+	 * @param upload 업로드 파일
+	 * @param user 로그인한 사용자 정보
+	 * @return 게시판 글 목록(메인)
+	 */
+	@PostMapping("write")
+	public String write(
+			BoardDTO boardDTO,
+			@RequestParam(name = "upload", required = false) MultipartFile upload,
+			@AuthenticationPrincipal UserDetails user
+			) {
+		
+		boardDTO.setMemberId(user.getUsername());
+		log.debug("저장할 글 정보: {}", boardDTO);
+		log.debug("업로드한 파일의 원본 이름: {}", upload.getOriginalFilename());
+		log.debug("파일 크기: {}", upload.getSize());
+		log.debug("파일이 비어 있는지 여부: {}", upload.isEmpty());
+		
+		try {
+			bs.write(boardDTO, uploadPath, upload);
+			log.debug("글 저장 성공!");
+			return "redirect:list";
+		} catch (Exception e) {
+			log.debug("[예외 발생] 글 저장 실패...");
+			return "board/writeForm";
+		}
+	}
+	
+	/**
+	 * 게시판 글 목록 페이지 이동
+	 * @param model
+	 * @return list.html
+	 */
+	@GetMapping("list")
+	public String list(Model model) {
+		
+		try {
+			List<BoardDTO> dto = bs.selectAll();
+			model.addAttribute("boardlist", dto);
+			log.debug("글 목록 불러오기 성공!");
+		} catch (Exception e) {
+			log.debug("[예외 발생] 글 목록 불러오기 실패..");
+		}
+		
+		return "board/list";
+	}
+	
+	@GetMapping("read")
+	public String readForm(@RequestParam(name="boardNum") int boardNum, Model model) {
+		
+		bs.viewCount();
+		
+		try {
+			BoardDTO dto = bs.find(boardNum);
+			model.addAttribute("board", dto);
+			log.debug("게시글 읽기 성공!");
+		} catch (Exception e) {
+			log.debug("[예외 발생] 게시글 읽기 실패..");
+		}
+		
+		return "board/readForm";
 	}
 }
--- springEx5/src/main/java/net/dsa/web5/service/BoardService.java
+++ springEx5/src/main/java/net/dsa/web5/service/BoardService.java
@@ -1,10 +1,20 @@
 package net.dsa.web5.service;
 
-import org.springframework.stereotype.Service;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.dsa.web5.dto.BoardDTO;
+import net.dsa.web5.entity.BoardEntity;
+import net.dsa.web5.entity.MemberEntity;
 import net.dsa.web5.repository.BoardRepository;
 import net.dsa.web5.repository.MemberRepository;
 import net.dsa.web5.repository.ReplyRepository;
@@ -22,6 +32,74 @@ public class BoardService {
 	private final BoardRepository br;
 	private final ReplyRepository rr;
 	private final FileManager fileManager;
+	
+	
+	/**
+	 * 게시판 글 저장
+	 * @param boardDTO
+	 * @param uploadPath
+	 * @param upload
+	 * @throws IOException 
+	 */
+	public void write(BoardDTO boardDTO, String uploadPath, MultipartFile upload) throws IOException {
+
+		// 글 작성자 정보 조회
+		MemberEntity memberEntity = mr.findById(boardDTO.getMemberId())
+										.orElseThrow(
+												() -> new EntityNotFoundException("회원이 없습니다.")
+												);
+										
+		// 게시글 저장
+		BoardEntity entity = BoardEntity.builder().build();
+		entity.setMember(memberEntity);
+		entity.setTitle(boardDTO.getTitle());
+		entity.setContents(boardDTO.getContents());
+		
+		// 첨부파일
+		if (upload != null && !upload.isEmpty()) {
+			String fileName;
+			fileName = fileManager.saveFile(uploadPath, upload);
+			entity.setFileName(fileName);
+			entity.setOriginalName(upload.getOriginalFilename());
+		}
+		
+		log.debug("저장되는 Entity: {}", entity);
+		br.save(entity);
+		
+	}
+
+	/**
+	 * 게시글 전체 조회
+	 * @return 글 목록
+	 */
+	public List<BoardDTO> selectAll() {
+		
+		Sort sort = Sort.by(Sort.Direction.DESC, "boardNum");
+		
+		List<BoardEntity> entityList = br.findAll(sort);
+		List<BoardDTO> dtoList = new ArrayList<>();
+
+		for (BoardEntity entity : entityList) {
+			BoardDTO dto = BoardDTO.convertToBoardDTO(entity);
+			dtoList.add(dto);
+		}
+		
+		return dtoList;
+	}
+
+	public BoardDTO find(int boardNum) {
+		
+		BoardEntity entity = br.findById(boardNum).orElseThrow(() -> new EntityNotFoundException());
+		
+		int viewCount = entity.getViewCount();
+		viewCount++;
+		entity.setViewCount(viewCount);
+		
+		BoardDTO dto = BoardDTO.convertToBoardDTO(entity);
+	
+		return dto;
+	}
+
 	
 	
 }
